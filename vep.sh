3b7f3bc22a1e569a48d35358caec8dcc7c665bc1
Elaine Lee <lee02326@umn.edu>
2021-11-17 13:11:08 -0600

    for barley

--- vep.sh
+++ vep.sh
@@ -14,22 +14,22 @@ module load perl/modules.centos7.5.26.1
 
 #    Variant sets should be either 'all', deletions', 'insertions', 'snps', 'common', or 'rare'
 VARIANT_SET=$1
-VCF=/scratch.global/lee02326/vep/$1/*.vcf.gz
-GFF=/scratch.global/lee02326/vep/$1/*.gff3.gz
-FASTA=/scratch.global/lee02326/vep/$1/*.fa.gz
+VCF=/scratch.global/lee02326/vep/barley/$2
+GFF=/scratch.global/lee02326/vep/barley/$3
+FASTA=/scratch.global/lee02326/vep/barley/$4
 
-OUT=/scratch.global/lee02326/vep/$1
+OUT=/scratch.global/lee02326/vep/barley/
 cd "${OUT}"
 
 /home/morrellp/shared/Software/ensembl-vep-release-97.3/vep \
     -i "${VCF}" \
     --gff "${GFF}" \
     --fasta "${FASTA}" \
-    --species cowpea \
+    --species barley \
     --total_length \
     --check_svs \
     --verbose \
     --format vcf \
     --force \
-    --warning_file /scratch.global/lee02326/vep/${1}/${VARIANT_SET}.txt \
-    -o /scratch.global/lee02326/vep/${1}/${VARIANT_SET}.txt
+    --warning_file /scratch.global/lee02326/vep/barley/"barley_${VARIANT_SET}.txt" \
+    -o /scratch.global/lee02326/vep/barley/"barley_${VARIANT_SET}.txt"
